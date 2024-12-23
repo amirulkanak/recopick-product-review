0bb685340883ad41c5021345f80cb918fd8899d2
amirulkanak <coder.kanak@gmail.com>
2024-12-24 04:10:44 +0600

    My Query Card and fetch data from server

---
+++ src/components/MyQueryCard.jsx
@@ -0,0 +1,59 @@
+const MyQueryCard = ({ query }) => {
+  return (
+    <div className="max-w-lg mx-auto bg-clr-accent/10 shadow-lg rounded-lg overflow-hidden border">
+      <div className="p-4">
+        <div className="flex items-center space-x-4">
+          <img
+            src={query.productImageURL}
+            alt={query.productName}
+            className="w-16 h-16 object-cover rounded-md border"
+          />
+          <div>
+            <h2 className="text-lg font-semibold">{query.productName}</h2>
+            <p className="text-sm text-gray-700">{query.productBrand}</p>
+          </div>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-md font-bold">{query.queryTitle}</h3>
+          <p className="text-sm text-gray-700 mt-2">
+            {query.boycottingReasonDetails}
+          </p>
+        </div>
+        <div className="mt-4 text-gray-600 text-sm">
+          <p className="text-base mb-3">
+            Recommendation: {query.recommendationCount}
+          </p>
+          <p className="mb-1">
+            Posted Date: {new Date(query.createdAt).toLocaleDateString()}
+          </p>
+          <p>
+            Posted Time:{' '}
+            {new Date(query.createdAt).toLocaleTimeString([], {
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
+          </p>
+        </div>
+        <div className="flex justify-end mt-4 space-x-3">
+          <button
+            // onClick={() => onViewDetails(query._id)}
+            className="bg-clr-secondary text-white px-4 py-2 rounded-md hover:bg-opacity-90">
+            View Details
+          </button>
+          <button
+            // onClick={() => onUpdate(query._id)}
+            className="bg-clr-primary-text/80 text-white px-4 py-2 rounded-md hover:hover:bg-opacity-100">
+            Update
+          </button>
+          <button
+            // onClick={() => onDelete(query._id)}
+            className="bg-red-500/90 text-white px-4 py-2 rounded-md hover:bg-red-600">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyQueryCard;
--- src/pages/MyQueriesPage.jsx
+++ src/pages/MyQueriesPage.jsx
@@ -1,17 +1,26 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import MyQueryCard from '../components/MyQueryCard';
+import LoadingSpinner from './../components/LoadingSpinner';
 
 const MyQueriesPage = () => {
   const [myQueries, setMyQueries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { user } = useAuth();
 
   useEffect(() => {
     const fetchMyQueries = async () => {
       try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_API_URL}/query/all`
+        const { data } = await axios.get(
+          `${import.meta.env.VITE_BACKEND_API_URL}/query/user/${user.email}`
         );
-        console.log(response.data);
+
+        setMyQueries(data.result);
+        if (data.result.length === 0) {
+          setLoading(false);
+        }
       } catch (error) {
         console.error('Error fetching queries:', error);
       }
@@ -20,12 +29,13 @@ const MyQueriesPage = () => {
     fetchMyQueries();
   }, []);
 
+  console.log(myQueries);
   return (
     <>
       {/* Banner */}
       <section className="relative bg-add-queries-bg bg-cover bg-center h-96">
         {/* Overlay */}
-        <div className="absolute inset-0 bg-emerald-700 bg-opacity-85"></div>
+        <div className="absolute inset-0 bg-emerald-700 bg-opacity-90"></div>
         {/* Content */}
         <div className="relative flex flex-col items-center justify-center text-center h-full px-6">
           <h1 className="text-4xl md:text-6xl text-clr-neutral font-bold mb-4">
@@ -44,6 +54,26 @@ const MyQueriesPage = () => {
       </section>
 
       {/* Queries Card In descending Order */}
+      <section className="max-width-wrapper my-20">
+        <h2 className="text-3xl font-semibold mb-10 text-center">
+          My Query List
+        </h2>
+        {myQueries.length === 0 ? (
+          <div className="flex items-center justify-center">
+            {loading ? (
+              <LoadingSpinner size={3} />
+            ) : (
+              ' Your List is empty. Please add some query.'
+            )}
+          </div>
+        ) : (
+          <div className="grid gap-5 w-fit mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center">
+            {myQueries.map((query) => (
+              <MyQueryCard key={query._id} query={query} />
+            ))}
+          </div>
+        )}
+      </section>
     </>
   );
 };
