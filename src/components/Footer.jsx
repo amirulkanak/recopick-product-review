18c1c94d75f945d4ec3d86d0599ba1e818f5e5da
amirulkanak <coder.kanak@gmail.com>
2025-01-11 12:24:16 +0600

    footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import recopickLogo from '../assets/icons/logo-recopick.png';
 
 const Footer = () => {
@@ -46,29 +46,29 @@ const Footer = () => {
                       </defs>
                     </svg>
                   </span>
-                  <span>+012 (345) 678 99</span>
+                  <span>+880 1913 015092</span>
                 </p>
               </div>
             </div>
 
             <LinkGroup header="Helpful Links">
-              <NavLink label="About Us" />
-              <NavLink label="Our Products" />
-              <NavLink label="Contact Us" />
-              <NavLink label="Careers" />
-              <NavLink label="FAQs" />
+              <CustomNavLink label="About Us" />
+              <NavLink to={'/queries'}>Queries</NavLink>
+              <CustomNavLink label="Contact Us" />
+              <CustomNavLink label="Careers" />
+              <CustomNavLink label="FAQs" />
             </LinkGroup>
             <LinkGroup header="Connect With Us">
-              <NavLink label="Events" />
-              <NavLink label="Testimonials" />
-              <NavLink label="Resources" />
-              <NavLink label="Support" />
+              <CustomNavLink label="Events" />
+              <CustomNavLink label="Testimonials" />
+              <CustomNavLink label="Resources" />
+              <CustomNavLink label="Support" />
             </LinkGroup>
             <LinkGroup header="Quick Links">
-              <NavLink label="Premium Support" />
-              <NavLink label="Our Services" />
-              <NavLink label="Know Our Team" />
-              <NavLink label="Download App" />
+              <CustomNavLink label="Premium Support" />
+              <CustomNavLink label="Our Services" />
+              <CustomNavLink label="Know Our Team" />
+              <CustomNavLink label="Download App" />
             </LinkGroup>
 
             <div className="w-full px-4 sm:w-1/2 lg:w-3/12">
@@ -76,40 +76,55 @@ const Footer = () => {
                 <h4 className="mb-9 text-lg font-semibold">Follow Us On</h4>
                 <ul className="mb-6 flex items-center">
                   <li className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-solid hover:bg-clr-accent hover:text-white  sm:mr-4 lg:mr-3 xl:mr-4">
-                    <svg
-                      width="8"
-                      height="16"
-                      viewBox="0 0 8 16"
-                      className="fill-current">
-                      <path d="M7.43902 6.4H6.19918H5.75639V5.88387V4.28387V3.76774H6.19918H7.12906C7.3726 3.76774 7.57186 3.56129 7.57186 3.25161V0.516129C7.57186 0.232258 7.39474 0 7.12906 0H5.51285C3.76379 0 2.54609 1.44516 2.54609 3.5871V5.83226V6.34839H2.10329H0.597778C0.287819 6.34839 0 6.63226 0 7.04516V8.90323C0 9.26452 0.243539 9.6 0.597778 9.6H2.05902H2.50181V10.1161V15.3032C2.50181 15.6645 2.74535 16 3.09959 16H5.18075C5.31359 16 5.42429 15.9226 5.51285 15.8194C5.60141 15.7161 5.66783 15.5355 5.66783 15.3806V10.1419V9.62581H6.13276H7.12906C7.41688 9.62581 7.63828 9.41935 7.68256 9.10968V9.08387V9.05806L7.99252 7.27742C8.01466 7.09677 7.99252 6.89032 7.85968 6.68387C7.8154 6.55484 7.61614 6.42581 7.43902 6.4Z" />
-                    </svg>
+                    <Link
+                      target="_blank"
+                      to="https://facebook.com/amirulalamkanak">
+                      <svg
+                        width="8"
+                        height="16"
+                        viewBox="0 0 8 16"
+                        className="fill-current">
+                        <path d="M7.43902 6.4H6.19918H5.75639V5.88387V4.28387V3.76774H6.19918H7.12906C7.3726 3.76774 7.57186 3.56129 7.57186 3.25161V0.516129C7.57186 0.232258 7.39474 0 7.12906 0H5.51285C3.76379 0 2.54609 1.44516 2.54609 3.5871V5.83226V6.34839H2.10329H0.597778C0.287819 6.34839 0 6.63226 0 7.04516V8.90323C0 9.26452 0.243539 9.6 0.597778 9.6H2.05902H2.50181V10.1161V15.3032C2.50181 15.6645 2.74535 16 3.09959 16H5.18075C5.31359 16 5.42429 15.9226 5.51285 15.8194C5.60141 15.7161 5.66783 15.5355 5.66783 15.3806V10.1419V9.62581H6.13276H7.12906C7.41688 9.62581 7.63828 9.41935 7.68256 9.10968V9.08387V9.05806L7.99252 7.27742C8.01466 7.09677 7.99252 6.89032 7.85968 6.68387C7.8154 6.55484 7.61614 6.42581 7.43902 6.4Z" />
+                      </svg>
+                    </Link>
                   </li>
                   <li className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-solid hover:bg-clr-accent hover:text-white y sm:mr-4 lg:mr-3 xl:mr-4">
-                    <svg
-                      width="16"
-                      height="12"
-                      viewBox="0 0 16 12"
-                      className="fill-current">
-                      <path d="M14.2194 2.06654L15.2 0.939335C15.4839 0.634051 15.5613 0.399217 15.5871 0.2818C14.8129 0.704501 14.0903 0.845401 13.6258 0.845401H13.4452L13.3419 0.751468C12.7226 0.258317 11.9484 0 11.1226 0C9.31613 0 7.89677 1.36204 7.89677 2.93542C7.89677 3.02935 7.89677 3.17025 7.92258 3.26419L8 3.73386L7.45806 3.71037C4.15484 3.61644 1.44516 1.03327 1.00645 0.587084C0.283871 1.76125 0.696774 2.88845 1.13548 3.59296L2.0129 4.90802L0.619355 4.20352C0.645161 5.18982 1.05806 5.96477 1.85806 6.52838L2.55484 6.99804L1.85806 7.25636C2.29677 8.45401 3.27742 8.94716 4 9.13503L4.95484 9.36986L4.05161 9.93346C2.60645 10.8728 0.8 10.8024 0 10.7319C1.62581 11.7652 3.56129 12 4.90323 12C5.90968 12 6.65806 11.9061 6.83871 11.8356C14.0645 10.2857 14.4 4.41487 14.4 3.2407V3.07632L14.5548 2.98239C15.4323 2.23092 15.7935 1.8317 16 1.59687C15.9226 1.62035 15.8194 1.66732 15.7161 1.6908L14.2194 2.06654Z" />
-                    </svg>
+                    <Link target="_blank" to="https://github.com/amirulkanak">
+                      <svg
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        className="fill-current">
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+                      </svg>
+                    </Link>
+                  </li>
+
+                  <li className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-solid hover:bg-clr-accent hover:text-white y sm:mr-4 lg:mr-3 xl:mr-4">
+                    <Link
+                      target="_blank"
+                      to="https://bd.linkedin.com/in/amirulalamkanak">
+                      <svg
+                        width="14"
+                        height="14"
+                        viewBox="0 0 14 14"
+                        className="fill-current">
+                        <path d="M13.0214 0H1.02084C0.453707 0 0 0.451613 0 1.01613V12.9839C0 13.5258 0.453707 14 1.02084 14H12.976C13.5432 14 13.9969 13.5484 13.9969 12.9839V0.993548C14.0422 0.451613 13.5885 0 13.0214 0ZM4.15142 11.9H2.08705V5.23871H4.15142V11.9ZM3.10789 4.3129C2.42733 4.3129 1.90557 3.77097 1.90557 3.11613C1.90557 2.46129 2.45002 1.91935 3.10789 1.91935C3.76577 1.91935 4.31022 2.46129 4.31022 3.11613C4.31022 3.77097 3.81114 4.3129 3.10789 4.3129ZM11.9779 11.9H9.9135V8.67097C9.9135 7.90323 9.89082 6.8871 8.82461 6.8871C7.73571 6.8871 7.57691 7.74516 7.57691 8.60323V11.9H5.51254V5.23871H7.53154V6.16452H7.55423C7.84914 5.62258 8.50701 5.08065 9.52785 5.08065C11.6376 5.08065 12.0232 6.43548 12.0232 8.2871V11.9H11.9779Z" />
+                      </svg>
+                    </Link>
                   </li>
                   <li className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-solid hover:bg-clr-accent hover:text-white y sm:mr-4 lg:mr-3 xl:mr-4">
-                    <svg
-                      width="16"
-                      height="12"
-                      viewBox="0 0 16 12"
-                      className="fill-current">
-                      <path d="M15.6645 1.88018C15.4839 1.13364 14.9419 0.552995 14.2452 0.359447C13.0065 6.59222e-08 8 0 8 0C8 0 2.99355 6.59222e-08 1.75484 0.359447C1.05806 0.552995 0.516129 1.13364 0.335484 1.88018C0 3.23502 0 6 0 6C0 6 0 8.79263 0.335484 10.1198C0.516129 10.8664 1.05806 11.447 1.75484 11.6406C2.99355 12 8 12 8 12C8 12 13.0065 12 14.2452 11.6406C14.9419 11.447 15.4839 10.8664 15.6645 10.1198C16 8.79263 16 6 16 6C16 6 16 3.23502 15.6645 1.88018ZM6.4 8.57143V3.42857L10.5548 6L6.4 8.57143Z" />
-                    </svg>
-                  </li>
-                  <li className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-solid hover:bg-clr-accent hover:text-white y sm:mr-4 lg:mr-3 xl:mr-4">
-                    <svg
-                      width="14"
-                      height="14"
-                      viewBox="0 0 14 14"
-                      className="fill-current">
-                      <path d="M13.0214 0H1.02084C0.453707 0 0 0.451613 0 1.01613V12.9839C0 13.5258 0.453707 14 1.02084 14H12.976C13.5432 14 13.9969 13.5484 13.9969 12.9839V0.993548C14.0422 0.451613 13.5885 0 13.0214 0ZM4.15142 11.9H2.08705V5.23871H4.15142V11.9ZM3.10789 4.3129C2.42733 4.3129 1.90557 3.77097 1.90557 3.11613C1.90557 2.46129 2.45002 1.91935 3.10789 1.91935C3.76577 1.91935 4.31022 2.46129 4.31022 3.11613C4.31022 3.77097 3.81114 4.3129 3.10789 4.3129ZM11.9779 11.9H9.9135V8.67097C9.9135 7.90323 9.89082 6.8871 8.82461 6.8871C7.73571 6.8871 7.57691 7.74516 7.57691 8.60323V11.9H5.51254V5.23871H7.53154V6.16452H7.55423C7.84914 5.62258 8.50701 5.08065 9.52785 5.08065C11.6376 5.08065 12.0232 6.43548 12.0232 8.2871V11.9H11.9779Z" />
-                    </svg>
+                    <Link target="_blank" to="https://t.me/amirulkanak">
+                      <svg
+                        width="16"
+                        height="16"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="fill-current">
+                        <path d="M12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0ZM17.4545 8.54545L15.8182 16.3636C15.7273 16.8182 15.4545 16.9091 15.0909 16.7273L12.2727 14.5455L10.9091 15.8182C10.8182 15.9091 10.7273 16 10.5455 16L10.7273 13.0909L15.0909 9.09091C15.2727 8.90909 15.0909 8.72727 14.9091 8.90909L8.72727 13.0909L5.90909 12.2727C5.45455 12.0909 5.45455 11.6364 6 11.4545L16.3636 7.63636C16.7273 7.54545 17.0909 7.81818 17.4545 8.54545Z" />
+                      </svg>
+                    </Link>
                   </li>
                 </ul>
                 <p className="text-base text-body-color dark:text-dark-6">
@@ -149,7 +164,7 @@ const LinkGroup = ({ children, header }) => {
   );
 };
 
-const NavLink = ({ label }) => {
+const CustomNavLink = ({ label }) => {
   return (
     <li className="inline-block text-base leading-loose text-body-color hover:text-primary dark:text-dark-6">
       {label}
